b485688b9195496731d006dee92b5c914a78f087
andrea vettydev <vettydevand@gmail.com>
2025-11-16 03:36:59 +0000

    Run npm run build
    > nextn@0.1.0 build
    > NODE_ENV=production next buil

--- next.config.ts
+++ next.config.ts
@@ -37,10 +37,13 @@ const nextConfig: NextConfig = {
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      // Don't resolve 'async_hooks' on the client
+      // Don't resolve server-side modules on the client
       config.resolve.fallback = {
         ...config.resolve.fallback,
         async_hooks: false,
+        fs: false,
+        tls: false,
+        net: false,
       };
     }
     return config;
